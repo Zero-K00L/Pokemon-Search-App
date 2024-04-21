5df54ad85cdaf4b06eda4007632af40664ee9ea2
John Grudzinski <johngrudzinski@yahoo.com>
2024-04-21 16:44:14 -0400

    JS update pokemon stats no fill in based on user input, bugs still present

--- script.js
+++ script.js
@@ -2,45 +2,54 @@ const userInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-button');
 const pokemonName = document.getElementById('pokemon-name');
 const pokemonId = document.getElementById('pokemon-id');
-const weight = document.getElementById('weight');
-const height = document.getElementById('height');
-const types = document.getElementById('types');
+const pokemonWeight = document.getElementById('weight');
+const pokemonHeight = document.getElementById('height');
+const typesContainer = document.getElementById('types');
 const hp = document.getElementById('hp');
 const attack = document.getElementById('attack');
 const defense = document.getElementById('defense');
-const specialAttack = document.getElementById('specialAttack');
-const specialDefense = document.getElementById('specialDefense');
+const specialAttack = document.getElementById('special-attack');
+const specialDefense = document.getElementById('special-defense');
 const speed = document.getElementById('speed');
 const spriteContainer = document.getElementById('sprite-container');
+
+const typesClasses = document.querySelectorAll('.type');
 
 const testBtn = document.getElementById('test-btn');
 
 const pokemonURL = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon'
 
 const fetchPokemonData = async () => {
+    let input = userInput.value.toLowerCase();
     try {
-        const res = await fetch(pokemonURL);
+        const res = await fetch(`${pokemonURL}/${input}/`);
         const data = await res.json();
         deconIdNameURL(data);
     }
     catch (err) {
         console.log(err);
+        alert('Pokemon not found');
     }
 };
 
 const deconIdNameURL = (data) => {
     let pokeToFind = userInput.value.toLowerCase();
-    const {results} = data;
-    
-    for(const pokemon of results) {
-        const {id, name, url} = pokemon;
-        if(Number(pokeToFind) === id || pokeToFind === name) {
-            console.log('Pokemon found:', name ,url);
-            return url;
-        }
-    }
-    console.log('Pokemon not found');
-    return null;
+    const {height, id, name, order, sprites, stats, types, weight} = data;
+    pokemonName.textContent = name.toUpperCase();
+    pokemonId.textContent = '#' + id;
+    pokemonHeight.textContent = 'height: '+ height;
+    pokemonWeight.textContent = 'weight: '+ weight;
+    spriteContainer.innerHTML = `<img class="sprite" src="${sprites['front_default']}" alt="an image of ${name}">`;
+    typesClasses.forEach((type, index) => {
+        type.textContent = types[index].type.name;
+        type.classList.add(`${types[index].type.name}`);
+    });
+    hp.textContent = stats[0].base_stat;
+    attack.textContent = stats[1].base_stat;
+    defense.textContent = stats[2].base_stat;
+    specialAttack.textContent = stats[3].base_stat;
+    specialDefense.textContent = stats[4].base_stat;
+    speed.textContent = stats[5].base_stat;
 };
 
 
@@ -71,33 +80,30 @@ fetchPokemonData();
 
 
 
+// ORIGINAL FETCH CODE UNMODIFIED
 
-/* const deconIdNameURL = (data) => {
-    let pokeToFind = userInput.value;
-    const {count, results} = data;
-    results.forEach((pokemon) => {
+/* const fetchPokemonData = async () => {
+    try {
+        const res = await fetch(pokemonURL);
+        const data = await res.json();
+        deconIdNameURL(data);
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+
+const deconIdNameURL = (data) => {
+    let pokeToFind = userInput.value.toLowerCase();
+    const {results} = data;
+    
+    for(const pokemon of results) {
         const {id, name, url} = pokemon;
         if(Number(pokeToFind) === id || pokeToFind === name) {
             console.log('Pokemon found:', name ,url);
             return url;
         }
-        else {
-            console.log('false');
-            return false;
-        }
-    });
-};
-
-
-
-
-const fetchPokemonDetails = async () => {
-    try {
-        const res = await fetch(pokemon.url);
-        const data = await res.json();
-        
     }
-    catch (err) {
-        console.log(err);
-    }
-} */
+    console.log('Pokemon not found');
+    return null;
+}; */
